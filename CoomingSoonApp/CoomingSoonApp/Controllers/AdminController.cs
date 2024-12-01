7e650dc86dc4d47b998079c4b4b1b570dec51ac0
Milad Asadi <46265665+Milad-PC@users.noreply.github.com>
2024-12-01 16:55:46 +0330

    login done

--- CoomingSoonApp/CoomingSoonApp/Controllers/AdminController.cs
+++ CoomingSoonApp/CoomingSoonApp/Controllers/AdminController.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace CoomingSoonApp.Controllers
 {
     public class AdminController : Controller
     {
+        private MyContext _db = new MyContext();
         // GET: Admin
         public ActionResult Index()
         {
@@ -21,18 +23,27 @@ namespace CoomingSoonApp.Controllers
         {
             return View();
         }
+        [AllowAnonymous]
         public ActionResult Login()
         {
             return View();
         }
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Login(string UserName , string Password)
+        [AllowAnonymous]
+        public ActionResult Login([Bind(Include = "UserName,Password")]Admins src)
         {
+            if(_db.Admins.Any(u=>u.UserName == src.UserName && u.Password == src.Password))
+            {
+                FormsAuthentication.SetAuthCookie(src.UserName,true);
+                return RedirectToAction("Index");
+            }
+            ViewBag.Error = "User Not Found";
             return View();
         }
         public ActionResult Signout()
         {
+            FormsAuthentication.SignOut();
             return RedirectToAction("Index", "Home");
         }
     }
