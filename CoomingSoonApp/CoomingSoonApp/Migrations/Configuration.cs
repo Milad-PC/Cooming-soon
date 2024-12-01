ea0c2df2d1313ce5bc1bf2a3425039d12ff8a9b5
Milad Asadi <46265665+Milad-PC@users.noreply.github.com>
2024-12-01 16:21:26 +0330

    add db

---
+++ CoomingSoonApp/CoomingSoonApp/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace CoomingSoonApp.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<CoomingSoonApp.MyContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(CoomingSoonApp.MyContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
---
+++ CoomingSoonApp/CoomingSoonApp/Migrations/MyContext.cs
@@ -0,0 +1,15 @@
+﻿using CoomingSoonApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace CoomingSoonApp
+{
+    public class MyContext : DbContext
+    {
+        public DbSet<Request> Requests { get; set; }
+        public DbSet<EmailReq> EmailRequests { get; set; }
+    }
+}
